2c0d2e5bdc129aab482c121c61ee558bb7d04522
janskiba <49987793+janskiba@users.noreply.github.com>
2021-03-22 20:37:29 +0100

    add arrow and animation on click

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,3 +2,13 @@
  * Custom code goes here.
  * A template should always ship with an empty custom.js
  */
+
+const target = document.querySelector(".banner-full-height a");
+
+target.addEventListener("click", function () {
+  var elem = document.querySelector(".featured-products");
+  window.scroll({
+    top: elem.offsetTop,
+    behavior: "smooth",
+  });
+});
--- modules/fullheightbanner/controllers/front/display.php
+++
@@ -1,9 +0,0 @@
-<?php
-class mymoduledisplayModuleFrontController extends ModuleFrontController
-{
-    public function initContent()
-    {
-        parent::initContent();
-        $this->setTemplate('module:fullheightbanner/views/templates/front/display.tpl');
-    }
-}
