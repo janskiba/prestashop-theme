a8838ce0a3ff5696c8ba05f1d8e83b33af3c4f81
janskiba <49987793+janskiba@users.noreply.github.com>
2021-03-25 13:34:36 +0100

    add animaton to feature poducts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,12 +3,50 @@
  * A template should always ship with an empty custom.js
  */
 
-const target = document.querySelector(".banner-full-height a");
+const click = document.querySelector(".banner-full-height a");
 
-target.addEventListener("click", function () {
-  var elem = document.querySelector(".featured-products");
+click.addEventListener("click", function () {
+  var elem = document.querySelector(".products-section-title");
   window.scroll({
     top: elem.offsetTop,
     behavior: "smooth",
+  });
+});
+
+(function ($) {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function (partial) {
+    var $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return compareBottom <= viewBottom && compareTop >= viewTop;
+  };
+})(jQuery);
+
+var win = $(window);
+var allMods = $(".featured-products");
+
+win.scroll(function (event) {
+  allMods.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
   });
 });
